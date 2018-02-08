492c14e2cfc6c2646604c577d4c10b0ec5e9a846
Rennan Francisco Messias de Lima <mrennan.lima@gmail.com>
2018-02-08 17:05:32 -0500

    teste - add validaçao para usuario ativo

--- sion-web/src/main/java/br/ufac/sion/security/UsuarioSistema.java
+++ sion-web/src/main/java/br/ufac/sion/security/UsuarioSistema.java
@@ -21,4 +21,10 @@ public class UsuarioSistema extends User {
         return usuario;
     }
 
+    @Override
+    public boolean isEnabled() {
+        return this.usuario.getAtivo();
+    }
+    
+
 }
